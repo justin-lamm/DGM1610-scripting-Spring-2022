3ae88e8e80686fadd62a254869ba47dd3e9d3a91
justin lam <10901528@my.uvu.edu>
2022-02-21 18:43:15 -0700

    if statement

--- Myfirstgame/Assets/scripts/assignments/variable conditionals/variables.cs
+++ Myfirstgame/Assets/scripts/assignments/variable conditionals/variables.cs
@@ -6,8 +6,11 @@ public class variables : MonoBehaviour
 {
     // Global Variables
     public int money; //Whole number value 2, 14, 57, -92, -101
+
     public float change = 0.75f; //Floating point number or decimal 3.15, 105.34, -299.87
+
     public bool isDay = true; //Booleans are true or false
+    
     private string loginName = "Bob2345";//String are alpha numeric text contained it quotation marks
     
 
