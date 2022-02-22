ebbedd50d6cfd319b26eb0e1507f05c4df4f776b
justin lam <10901528@my.uvu.edu>
2022-02-21 19:56:47 -0700

    Created Camera Follow and Refactored Player Controller

--- Myfirstgame/Assets/scripts/PlayerController.cs
+++ Myfirstgame/Assets/scripts/PlayerController.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20.0f;
-    public float turnspeed = 50.0f;
-    public float hInput;
-    public float fInput;
+    private float speed = 20.0f;
+    private float turnspeed = 50.0f;
+    private float hInput;
+    private float fInput;
 
-    // Start is called before the first frame update
 
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -22,8 +17,8 @@ public class PlayerController : MonoBehaviour
         hInput = Input.GetAxis("Horizontal");
         fInput = Input.GetAxis("Vertical");
 
-        // Makes the player move forward
+        // Makes the player move 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnspeed * hInput);
+        transform.Rotate(Vector3.up * turnspeed * Time.deltaTime * hInput);
     } 
 }
